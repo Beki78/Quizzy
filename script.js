b97ead3d4c20adda2cdd40d4cdf81927d34adaf1
Bereket Getachew <146061009+Beki78@users.noreply.github.com>
2024-02-23 21:42:16 +0300

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,99 @@
+const quizQuestions = [
+  {
+    question: "What is the name of the tallest mountain in the world?",
+    a: "Mount Everest",
+    b: "Killi mangaro",
+    c: "Dahhin terara",
+    d: "Amazon",
+    correct: "a",
+  },
+  {
+    question: "Which country has the largest population in the world?",
+    a: "Australia",
+    b: "USA",
+    c: "Africa",
+    d: "China",
+    correct: "d",
+  },
+  {
+    question: "What is the name of the longest river in Africa?",
+    a: "Bantu",
+    b: "Red sea",
+    c: " The Nile River",
+    d: "Amazon",
+    correct: "c",
+  },
+  {
+    question: "What American city is the Golden Gate Bridge located in?",
+    a: "Texas",
+    b: "California",
+    c: "San Francisco",
+    d: "Nevada",
+    correct: "c",
+  },
+  {
+    question: "What is the capital of Mexico?",
+    a: "Sans",
+    b: "Mehico",
+    c: "Pablo mari",
+    d: "Mexico City",
+    correct: "d",
+  },
+  {
+    question: "What is the name of the largest country in the world?",
+    a: "Russia",
+    b: "USA",
+    c: "Africa",
+    d: "India",
+    correct: "a",
+  },
+];
+
+const geography = document.querySelector(".topic");
+const questionEl = document.getElementById("question");
+const chooseA = document.getElementById("chooseA");
+const chooseB = document.getElementById("chooseB");
+const chooseC = document.getElementById("chooseC");
+const chooseD = document.getElementById("chooseD");
+const submitButton = document.getElementById("submitButton");
+const endScreen = document.getElementById("endScreen");
+const choose = document.getElementById("choose");
+const count = document.getElementById("count");
+const questionLength = document.getElementById("questionLength");
+const resultLength = document.getElementById("resultLength");
+const refreshButton = document.getElementById("restart");
+let currentQuiz = 0;
+function loadGeograph() {
+  currentQuizData = quizQuestions[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+
+  chooseA.innerText = currentQuizData.a;
+  chooseB.innerText = currentQuizData.b;
+  chooseC.innerText = currentQuizData.c;
+  chooseD.innerText = currentQuizData.d;
+  count.innerText = currentQuiz;
+  questionLength.innerText = quizQuestions.length;
+  resultLength.innerText = quizQuestions.length;
+}
+
+submitButton.addEventListener("click", () => {
+  currentQuiz++;
+
+  if (currentQuiz < quizQuestions.length) {
+    loadGeograph();
+  } else {
+    endScreen.style.display = "block";
+  }
+});
+
+choose.addEventListener("click", (e) => {
+  alert("clicked");
+});
+ refreshButton.addEventListener("click", function () {
+   // Refresh the page
+   location.reload();
+ });
 const spinWrapper = document.querySelector(".spinner-wrapper");
 window.addEventListener("load", () => {
   spinWrapper.style.opacity = "0";
@@ -12,11 +108,11 @@ topic.addEventListener("click", (e) => {
   window.location.href = "question.html";
 });
 
-const back = document.querySelector(".NavBarleft");
+const back = document.querySelector("#back");
 back.addEventListener("click", (e) => {
   window.location.href = "index.html";
-  console.log("sd");
 });
+loadGeograph();
 
 let icon = document.querySelector(".icon");
 function changeTheme() {
@@ -36,5 +132,4 @@ function changeTheme() {
     icon.classList.remove("fa-sun");
     icon.classList.add("fa-moon");
   }
-  
 }
